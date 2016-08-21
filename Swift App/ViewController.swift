1aaff3da8f48558e66903ebabd70d9f36763f98d
Braden Xcode <braden_gile@yahoo.com>
2016-08-21 15:56:33 -0700

    2

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -10,9 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var CoolLabel: UILabel!
+    
+    var TapCount = 0
+   
+    @IBAction func ButtonTapped(_ sender: AnyObject){
+        
+       TapCount = TapCount + 1
+       
+        if TapCount >= 10 {
+            CoolLabel.text = "Your Dumbass Tapped A Worthless Piece Of Shit Button, Congratulations"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
